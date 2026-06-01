e4bc85e18d1fc8c5d911a4b2836da2f6fb77864e
Lawenignina <mabdouldareini01@gmail.com>
2026-06-01 09:53:00 -0400

    Remove books controller from module

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { BooksController } from './books.controller';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  controllers: [BooksController],
   providers: [PrismaService],
 })
 export class BooksModule {}
