aba9d89cb839750d4a02ca0f5abc9c6daa88b4c2
Lawenignina <mabdouldareini01@gmail.com>
2026-05-31 21:33:08 -0400

    Fix nodemailer SMTP config

--- dist/auth/otp.service.js
+++ dist/auth/otp.service.js
@@ -22,7 +22,9 @@ let OtpService = class OtpService {
     }
     async sendOtp(email, otp) {
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.GMAIL_USER,
                 pass: process.env.GMAIL_PASSWORD,
--- src/auth/otp.service.ts
+++ src/auth/otp.service.ts
@@ -12,7 +12,9 @@ export class OtpService {
 
   async sendOtp(email: string, otp: string) {
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASSWORD,
